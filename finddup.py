665f433adedca0a85f13d61e414542ddc8dc03a9
Andrés Delfino <adelfino@gmail.com>
2018-04-16 22:53:17 -0300

    Introducing Powerwalking

--- finddup.py
+++ finddup.py
@@ -1,5 +1,6 @@
 import hashlib
 import os
+import os.path
 import sys
 
 
@@ -13,40 +14,43 @@ def get_hash(path):
     return hasher.hexdigest()
 
 
+def powerwalk(top):
+    with os.scandir(top) as it:
+        for entry in it:
+            if entry.is_dir():
+                yield from powerwalk(entry.path)
+            else:
+                yield entry
+
+
 def get_duplicate_files(path):
     """Returns a list of duplicate files of a directory in the form of a hash-keyed dictionary"""
 
-    def dup(path, paths_per_hash, hashes_per_size, firsts_of_size, sizes_found):
-        with os.scandir(path) as it:
-            for entry in it:
-                if entry.is_dir():
-                    dup(entry.path, paths_per_hash, hashes_per_size, firsts_of_size, sizes_found)
-                    continue
-
-                file_size = entry.stat().st_size
-                if file_size == 0:
-                    continue
-
-                if file_size not in sizes_found:
-                    sizes_found.append(file_size)
-                    firsts_of_size[file_size] = entry.path
-                    continue
-
-                if file_size not in hashes_per_size:
-                    prev_hash = get_hash(firsts_of_size[file_size])
-                    hashes_per_size[file_size] = [prev_hash]
-                    paths_per_hash[prev_hash] = [firsts_of_size[file_size]]
-
-                file_hash = get_hash(entry.path)
-
-                if file_hash not in hashes_per_size[file_size]:
-                    hashes_per_size[file_size].append(file_hash)
-                    paths_per_hash[file_hash] = [entry.path]
-                else:
-                    paths_per_hash[file_hash].append(entry.path)
-
     paths_per_hash = {}
-    dup(path, paths_per_hash, {}, {}, [])
+    hashes_per_size = {}
+    firsts_of_size = {}
+
+    for entry in powerwalk(path):
+        file_size = entry.stat().st_size
+        if file_size == 0:
+            continue
+
+        if file_size not in firsts_of_size:
+            firsts_of_size[file_size] = entry.path
+            continue
+
+        if file_size not in hashes_per_size:
+            prev_hash = get_hash(firsts_of_size[file_size])
+            hashes_per_size[file_size] = [prev_hash]
+            paths_per_hash[prev_hash] = [firsts_of_size[file_size]]
+
+        file_hash = get_hash(entry.path)
+
+        if file_hash not in hashes_per_size[file_size]:
+            hashes_per_size[file_size].append(file_hash)
+            paths_per_hash[file_hash] = [entry.path]
+        else:
+            paths_per_hash[file_hash].append(entry.path)
 
     duplicate_files = {}
     for file_hash, paths in paths_per_hash.items():
@@ -57,14 +61,9 @@ def get_duplicate_files(path):
 
 
 if __name__ == '__main__':
-    if sys.argv[1] == '--delete':
-        delete = True
-    else:
-        delete = False
-
     duplicate_files = get_duplicate_files(sys.argv[-1])
 
-    if not delete:
+    if not sys.argv[1] == '--delete':
         for file_hash in duplicate_files:
             for path in duplicate_files[file_hash]:
                 print(f'{file_hash}: {path}')
